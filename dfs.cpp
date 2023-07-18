cd5d200d9dcf9a8dc23d2b6263940f968c732fe8
dhruv1345 <21102156@mail.jiit.ac.in>
2023-07-18 11:34:07 +0530

    Graphs: DFS code

--- dfs.cpp
+++ dfs.cpp
@@ -3,6 +3,11 @@
 #include<queue>
 using namespace std;
 
+// Time Complexity: For an undirected graph, O(N) + O(2E), For a directed graph, O(N) + O(E), Because for every node we are calling the recursive function once, the time taken is O(N) and 2E is for total degrees as we traverse for all adjacent nodes.
+
+// Space Complexity: O(3N) ~ O(N), Space for dfs stack space, visited array and an adjacency list.
+
+
 class Solution{
     private:
     void dfsOfGraph(int node, vector<int> adj[], int vis[], vector<int> &ls) {
